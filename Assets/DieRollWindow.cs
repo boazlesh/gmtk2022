95c87c5516432a6c1335a8047e604b8e91949e0c
Moti Baral <animamaster999@gmail.com>
2022-07-16 11:42:56 +0300

    added a roll window

---
+++ Assets/DieRollWindow.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+namespace Assets
+{
+    public class DieRollWindow : MonoBehaviour
+    {
+        [SerializeField] private FaceColor _faceColor;
+        [SerializeField] private Die _diePrefab;
+        [SerializeField] private Transform _content;
+
+        public void AddDie(DieFace dieFace)
+        {
+            Die die = Instantiate(_diePrefab, _content);
+
+            die.SetColor(_faceColor);
+            die.SetFace(dieFace);
+        }
+
+        [ContextMenu("Add Die")]
+        public void AddDieDebug()
+        {
+            AddDie(DieFace.Five);
+        }
+    }
+}
