3a45c77985a8f7a0086dcdcdf7e9fb9f514f617b
boazlesh <boazlesh@gmail.com>
2022-07-16 19:52:38 +0300

    Health idk

---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private HealthDisplay _healthDisplay;
+
+    private int _currentHealth;
+    public int CurrentHealth
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        set
+        {
+            _currentHealth = value;
+            _healthDisplay?.SetText(_currentHealth);
+        }
+    }
+
+    private void Start()
+    {
+        CurrentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth = Mathf.Max(0, CurrentHealth - damage);
+
+        if (CurrentHealth == 0)
+        {
+            Die();
+        }
+    }
+
+    [ContextMenu("Take 1 Damage")]
+    public void TakeDamageEditor()
+    {
+        TakeDamage(1);
+    }
+
+    public void Heal(int heal)
+    {
+        CurrentHealth = Mathf.Min(_maxHealth, CurrentHealth + heal);
+    }
+
+    private void Die()
+    {
+
+    }
+}
---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _healthText;
+
+    public void SetText(int health)
+    {
+        _healthText.SetText(health.ToString());
+    }
+}
