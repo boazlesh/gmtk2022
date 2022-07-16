832f6d6668c0bf9cc743df269295e506bbc00716
boazlesh <boazlesh@gmail.com>
2022-07-16 12:04:56 +0300

    cube can move

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private readonly Vector2 _toWorldMultiplier = new Vector2(2.0f, -1.5f);
+
+    private readonly GameObject[,] _matrix = new GameObject[3, 3];
+    private readonly Vector2Int _matrixSize;
+
+    public Board()
+    {
+        _matrixSize = new Vector2Int(_matrix.GetLength(0) - 1, _matrix.GetLength(1) - 1);
+    }
+
+    public Vector2 BoardPositionToWorldPosition(Vector2Int boardPosition)
+    {
+        return boardPosition * _toWorldMultiplier;
+    }
+
+    public Vector2Int GetMoveAttemptPosition(Vector2Int startPosition, Direction direction)
+    {
+        Vector2Int movement = GetMovement(direction);
+
+        Vector2Int resultPosition = startPosition + movement;
+
+        resultPosition.Clamp(Vector2Int.zero, _matrixSize);
+
+        return resultPosition;
+    }
+
+    private Vector2Int GetMovement(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return Vector2Int.down;
+            case Direction.Down:
+                return Vector2Int.up;
+            case Direction.Left:
+                return Vector2Int.left;
+            case Direction.Right:
+                return Vector2Int.right;
+            default:
+                Debug.LogError($"{nameof(GetMovement)} with unknown direction: {direction}");
+                return Vector2Int.zero;
+        }
+    }
+}
--- Assets/Scripts/CubeGuyLogic.cs
+++ Assets/Scripts/CubeGuyLogic.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeGuyLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Board _board;
+    [SerializeField] private SpriteRenderer _spriteTop;
+    [SerializeField] private SpriteRenderer _spriteFront;
+    [SerializeField] private SpriteRenderer _spriteSide;
+
+    private Vector2Int _boardPosition;
+    private Input _input;
+
+    public void Awake()
     {
-        
+        _input = new Input();
+        _input.Enable();
+
+        _input.BattleActionMap.MoveUp.performed += _ => Move(Direction.Up);
+        _input.BattleActionMap.MoveDown.performed += _ => Move(Direction.Down);
+        _input.BattleActionMap.MoveLeft.performed += _ => Move(Direction.Left);
+        _input.BattleActionMap.MoveRight.performed += _ => Move(Direction.Right);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move(Direction direction)
     {
-        
+        Vector2Int movePosition = _board.GetMoveAttemptPosition(_boardPosition, direction);
+
+        _boardPosition = movePosition;
+        transform.localPosition = _board.BoardPositionToWorldPosition(_boardPosition);
     }
 }
