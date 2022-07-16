103d44ff51fed6c85dff08e5631c04bd4adc3154
Moti Baral <animamaster999@gmail.com>
2022-07-16 11:57:50 +0300

    replaced all roll windows with prefabs

--- Assets/DieRollWindow.cs
+++ Assets/DieRollWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts;
+using TMPro;
 using UnityEngine;
 
 namespace Assets
@@ -8,6 +9,32 @@ namespace Assets
         [SerializeField] private FaceColor _faceColor;
         [SerializeField] private Die _diePrefab;
         [SerializeField] private Transform _content;
+        [SerializeField] private TextMeshProUGUI _sumLabel;
+
+        private int _sum = 0;
+
+        private void Awake()
+        {
+            foreach (Transform child in _content)
+            {
+                Destroy(child.gameObject);
+            }
+
+            _sumLabel.text = "0";
+        }
+
+        private void OnValidate()
+        {
+            foreach (Transform child in _content)
+            {
+                Die die = child.GetComponent<Die>();
+
+                if (die != null)
+                {
+                    die.SetColor(_faceColor);
+                }
+            }
+        }
 
         public void AddDie(DieFace dieFace)
         {
@@ -15,6 +42,10 @@ namespace Assets
 
             die.SetColor(_faceColor);
             die.SetFace(dieFace);
+
+            _sum += DieFaceHelper.GetNumericValue(dieFace);
+
+            _sumLabel.text = _sum.ToString();
         }
 
         [ContextMenu("Add Die")]
--- Assets/Scripts/DieFace.cs
+++ Assets/Scripts/DieFace.cs
@@ -10,4 +10,28 @@
         Five,
         Six
     }
+
+    public static class DieFaceHelper
+    {
+        public static int GetNumericValue(DieFace dieFace)
+        {
+            switch (dieFace)
+            {
+                case DieFace.One:
+                    return 1;
+                case DieFace.Two:
+                    return 2;
+                case DieFace.Three:
+                    return 3;
+                case DieFace.Four:
+                    return 4;
+                case DieFace.Five:
+                    return 5;
+                case DieFace.Six:
+                    return 6;
+                default:
+                    return 0;
+            }
+        }
+    }
 }
