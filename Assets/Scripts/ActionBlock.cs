b2959f37755c926c17469f212f7b34bf8fd1009d
Moti Baral <animamaster999@gmail.com>
2022-07-16 10:13:20 +0300

    3 actions + prefab

--- Assets/Scripts/ActionBlock.cs
+++ Assets/Scripts/ActionBlock.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionBlock : MonoBehaviour
 {
+    [SerializeField] private Sprite _actionSprite;
+    [SerializeField] private Color _color;
+    [SerializeField] private Image _actionImage;
+    [SerializeField] private Image _outlineImage;
+
+    private void OnValidate()
+    {
+        _actionImage.sprite = _actionSprite;
+        _outlineImage.color = _color;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
