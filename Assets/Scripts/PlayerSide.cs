6788996b9cd02ff19b8e8642fb0ee30ea9bb7442
Moti Baral <animamaster999@gmail.com>
2022-07-17 15:23:32 +0300

    change the board color

---
+++ Assets/Scripts/PlayerSide.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerSide : MonoBehaviour
+    {
+        [SerializeField] private Color _color;
+
+        private void OnValidate()
+        {
+            foreach (var spriteRenderer in GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.color = _color;
+            }
+        }
+    }
+}
