ae9cadbe24876dfedc52316e4b60971005839166
Moti Baral <animamaster999@gmail.com>
2022-07-17 12:41:16 +0300

    instructions music

--- Assets/Scripts/Instructions.cs
+++ Assets/Scripts/Instructions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Assets.Scripts
@@ -6,6 +7,7 @@ namespace Assets.Scripts
     public class Instructions : MonoBehaviour
     {
         [SerializeField] private LevelLoader _levelLoader;
+        [SerializeField] private AudioSource _audioSource;
 
         private Input _input;
 
@@ -30,7 +32,23 @@ namespace Assets.Scripts
 
         private void ContinuePerformed(InputAction.CallbackContext obj)
         {
+            StartCoroutine(FadeOut(1f));
             _levelLoader.LoadNextLevel();
+        }
+
+        public IEnumerator FadeOut(float fadeTimeSeconds)
+        {
+            float startVolume = _audioSource.volume;
+
+            while (_audioSource.volume > 0)
+            {
+                _audioSource.volume -= startVolume * Time.deltaTime / fadeTimeSeconds;
+
+                yield return null;
+            }
+
+            _audioSource.Stop();
+            _audioSource.volume = startVolume;
         }
     }
 }
