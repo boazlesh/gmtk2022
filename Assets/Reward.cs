09b3778af5dd600180b9db203371ee55c525819b
Moti Baral <animamaster999@gmail.com>
2022-07-17 19:05:44 +0300

    added rewards

---
+++ Assets/Reward.cs
@@ -0,0 +1,10 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Reward : MonoBehaviour
+    {
+        [SerializeField] public ActionModel _actionModel;
+    }
+}
--- Assets/Scripts/WinWindow.cs
+++ Assets/Scripts/WinWindow.cs
@@ -1,15 +1,55 @@
-﻿using Assets.Scripts;
+﻿using Assets;
+using Assets.Scripts;
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class WinWindow : MonoBehaviour
 {
     [SerializeField] private float _delay;
+    [SerializeField] private Image _image;
+    [SerializeField] private TextMeshProUGUI _name;
+    [SerializeField] private TextMeshProUGUI _description;
+    [SerializeField] private Transform _rewardWindow;
+
+    private Input _input;
 
     private void Awake()
     {
         FindObjectOfType<CubeGuyLogic>().enabled = false;
 
+        var reward = FindObjectOfType<Reward>();
+
+        if (reward != null)
+        {
+            _rewardWindow.gameObject.SetActive(true);
+
+            var action = reward._actionModel;
+
+            _image.sprite = action._sprite;
+            _name.text = action._displayName;
+            _description.text = action._text;
+        }
+
+        _input = new Input();
+
+        _input.BattleActionMap.Continue.performed += ContinuePerformed;
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+
+    private void ContinuePerformed(InputAction.CallbackContext obj)
+    {
         StartCoroutine(NextSceneCoroutine());
     }
 
