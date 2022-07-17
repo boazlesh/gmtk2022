a55ce5fe4d328088ff8bab7adb474ab0d41d2177
boazlesh <boazlesh@gmail.com>
2022-07-17 15:40:58 +0300

    lose window

---
+++ Assets/Scripts/WindowLose.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WindowLose : MonoBehaviour
+{
+    private Input _input;
+
+    private void Awake()
+    {
+        Time.timeScale = 0f;
+
+        var player = FindObjectOfType<CubeGuyLogic>();
+        if (player != null)
+        {
+            player.enabled = false;
+        }
+
+        _input = new Input();
+        _input.Enable();
+        _input.BattleActionMap.UseTopAbility.performed += _ => RestartLevel();
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        _input.Disable();
+        PauseMenu.RestartLevel();
+    }
+}
